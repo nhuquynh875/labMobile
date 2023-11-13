82367ff615e66a0ff6ed1edf68b770d6e32e5c74
nhuquynh875 <puddingchan268@gmail.com>
2023-11-13 22:25:39 +0700

    add main

--- basic-android-kotlin-compose-training-bus-schedule-app/app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.busschedule
-
-import android.app.Application
-import androidx.room.Database
-import com.example.busschedule.data.AppDatabase
-
-class BusScheduleApplication: Application() {
-    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
-}
