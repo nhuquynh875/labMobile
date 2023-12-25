8a9db949f1a8842d14c946a8aac0793a8e4b0fa7
nhuquynh875 <puddingchan268@gmail.com>
2023-12-25 21:42:13 +0700

    update

--- shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardRecyclerViewAdapter.kt
+++ shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardRecyclerViewAdapter.kt
@@ -3,14 +3,13 @@ package com.google.codelabs.mdc.kotlin.shrine
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.google.codelabs.mdc.kotlin.shrine.network.ImageRequester
 
 import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
 
 /**
  * Adapter used to show a simple grid of products.
  */
-class ProductCardRecyclerViewAdapter internal constructor(private val productList: List<ProductEntry>) : RecyclerView.Adapter<ProductCardViewHolder>() {
+class ProductCardRecyclerViewAdapter(private val productList: List<ProductEntry>) : RecyclerView.Adapter<ProductCardViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductCardViewHolder {
         val layoutView = LayoutInflater.from(parent.context).inflate(R.layout.shr_product_card, parent, false)
@@ -18,12 +17,7 @@ class ProductCardRecyclerViewAdapter internal constructor(private val productLis
     }
 
     override fun onBindViewHolder(holder: ProductCardViewHolder, position: Int) {
-        if (position < productList.size) {
-            val product = productList[position]
-            holder.productTitle.text = product.title
-            holder.productPrice.text = product.price
-            ImageRequester.setImageFromUrl(holder.productImage, product.url)
-        }
+        // TODO: Put ViewHolder binding code here in MDC-102
     }
 
     override fun getItemCount(): Int {
